8a3bfa970457629670b96705f6c5644b4e5e807e
Jieun <wldms941114@gmail.com>
2022-11-22 00:23:42 +0900

    UI속성변경

--- app/src/main/java/com/example/uikotlinpratice_zeeeni/MainActivity.kt
+++ app/src/main/java/com/example/uikotlinpratice_zeeeni/MainActivity.kt
@@ -2,13 +2,26 @@ package com.example.uikotlinpratice_zeeeni
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+//        확인 버튼이 눌리면? 이벤트를 달아주자.
 
+        okBtn.setOnClickListener {
+
+//            입력된 내용? contentEdt의 text의 값 조회 (get) 변수에 저장해두자.
+
+            val inputContent = contentEdt.text.toString()
+
+//            저장해둔 입력값을 => 텍스트뷰의 내용으로 반영. (set)
+
+            resultTxt.text = inputContent
+
+        }
 
     }
 }
